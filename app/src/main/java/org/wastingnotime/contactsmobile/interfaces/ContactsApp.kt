224fec3f296dbc7616c0706ca63cb6e7846112b4
hriccio <hriccio@users.noreply.github.com>
2026-05-14 17:38:48 -0300

    fix: follow system dark theme

--- app/src/main/java/org/wastingnotime/contactsmobile/interfaces/ContactsApp.kt
+++ app/src/main/java/org/wastingnotime/contactsmobile/interfaces/ContactsApp.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
@@ -20,6 +21,7 @@ import org.wastingnotime.contactsmobile.interfaces.ui.ContactsViewModel
 
 @Composable
 fun ContactsApp(viewModel: ContactsViewModel) {
+    val darkTheme = isSystemInDarkTheme()
     ContactsAppContent(
         uiState = null,
         detailUiState = null,
@@ -43,7 +45,7 @@ fun ContactsApp(viewModel: ContactsViewModel) {
         onEditPhoneNumberChange = {},
         onEditSubmit = {},
         onEditOpenContact = {},
-        darkTheme = false,
+        darkTheme = darkTheme,
         viewModel = viewModel,
     )
 }
